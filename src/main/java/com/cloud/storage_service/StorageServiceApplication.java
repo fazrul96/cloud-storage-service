862642e88b618a6b278c25876d1a40a96c85a6dc
Fazrul <mfazrul07@gmail.com>
2026-01-29 10:17:34 +0800

    Major refactor microservices

--- src/main/java/com/cloud/storage_service/StorageServiceApplication.java
+++ src/main/java/com/cloud/storage_service/StorageServiceApplication.java
@@ -3,9 +3,11 @@ package com.cloud.storage_service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @Slf4j
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "com.cloud.storage_service.properties")
 @SuppressWarnings({"PMD.UseUtilityClass"})
 public class StorageServiceApplication {
 
--- src/main/java/com/cloud/storage_service/config/CorsConfig.java
+++ src/main/java/com/cloud/storage_service/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.cloud.storage_service.config;
 
+import com.cloud.storage_service.properties.CorsProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,23 +12,24 @@ import java.util.List;
 
 import static com.cloud.storage_service.constants.GeneralConstant.DOUBLE_ASTERISKS;
 import static com.cloud.storage_service.constants.GeneralConstant.SLASH;
-import static com.cloud.storage_service.constants.SecurityConstant.*;
 
 @Configuration
+@RequiredArgsConstructor
 public class CorsConfig {
     public static final String WILDCARD_PATH = SLASH + DOUBLE_ASTERISKS;
+    private final CorsProperties corsProperties;
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(ALLOWED_ORIGINS);
-        config.setAllowedMethods(ALLOWED_METHODS);
-        config.setAllowedHeaders(ALLOWED_HEADERS);
-        config.setAllowCredentials(true);
+        config.setAllowedOrigins(corsProperties.getAllowedOrigins());
+        config.setAllowedMethods(corsProperties.getAllowedMethods());
+        config.setAllowedHeaders(corsProperties.getAllowedHeaders());
+        config.setAllowCredentials(corsProperties.isAllowCredentials());
         config.setExposedHeaders(List.of(
                 "Content-Disposition"
         ));
-        config.setMaxAge(3600L);
+        config.setMaxAge(corsProperties.getMaxAge());
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration(WILDCARD_PATH, config);
